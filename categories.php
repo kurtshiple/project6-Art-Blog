ead70e056701f060677f8c3d393047f61d4b0489
Kurt Shiple <kurtshiple@kurts-air.attlocal.net>
2019-09-25 19:14:24 -0400

    Added a categories page and connected to mysql db

---
+++ categories.php
@@ -0,0 +1,147 @@
+<?php require_once("includes/DB.php"); ?>
+<?php require_once("includes/Functions.php"); ?>
+<?php if(isset($_POST["Submit"])){
+    $Category = $_POST["CategoryTitle"]
+        
+    if(empty($Category)){
+        Error = "All Fields Must Be Filled Out";
+        Redirect_to("categories.php");
+        
+    }
+}
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="https://use.fontawesome.com/releases/v5.0.11/js/all.js"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/styles.css">
+    <title>Categories</title>
+</head>
+<body>
+    <!-- NAVBAR -->
+    <div style="height: 10px; background: #27aae1;"></div>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container">
+            <a href="#" class="navbar-brand">MARYSART.COM</a>
+            <button class="navbar-toggler" data-toggle="collapse" data-target="#navbarcollapseCMS">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarcollapseCMS">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item">
+                    <a href="MyProfile.php" class="nav-link"><i class="fas fa-user"></i> My Profile</a>
+                </li>
+                <li class="nav-item">
+                    <a href="Posts.php" class="nav-link"><i class="fas fa-align-left"></i> Posts</a>
+                </li>
+                <li class="nav-item">
+                    <a href="Categories.php" class="nav-link"><i class="fas fa-th-large"></i> Categories</a>
+                </li>
+                <li class="nav-item">
+                    <a href="Admins.php" class="nav-link"><i class="fas fa-users-cog"></i> Manage Admins</a>
+                </li>
+                <li class="nav-item">
+                    <a href="Comments.php" class="nav-link"><i class="fas fa-comments"></i> Comments</a>
+                </li>
+                <li class="nav-item">
+                    <a href="Blog.php?page=1" class="nav-link"><i class="fab fa-elementor"></i> Live Blog</a>
+                </li>
+            </ul>
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item"><a href="Logout.php" class="nav-link text-danger"><i class="fas fa-sign-out-alt"></i> Logout</a></li>
+            </ul>
+            </div>
+        </div>
+    </nav>
+    <div style="height: 10px; background: #27aae1;"></div>
+    <!-- NAVBAR END -->
+
+    <!--- HEADER -->
+    <header class="bg-dark text-white py-5">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                <h1><i class="fas fa-edit" style="color:#27aae1;"></i> Manage Categories</h1>
+                </div>
+            </div>
+        </div>
+    </header>
+    <!--- HEADER END -->
+    <br>
+    <!--- Main Area -->
+    
+    <section class="container py-2 mb-4">
+        <div class="row">
+            <div class="offset-lg-1 col-lg-10" style="min-height: 450px;">
+                <form class="" action="categories.php" method="post">
+                    <div class="card bg-secondary text-light mb-3 mt-3">
+                        <div class="card-header">
+                            <h1>
+                            Add New Category
+                            </h1>
+                        </div>
+                        <div class="card-body bg-dark">
+                            <div class="form-group">
+                                <label for="Title">
+                                    <span class="FieldInfo">
+                                    Category Title:
+                                    </span>
+                                </label>
+                                <input class="form-control" type:"text" name="CategoryTitle" id="title" placeholder="Type title here" value="">
+                            </div>
+                            <div class="row">
+                                <div class="col-lg-6 mb-2">
+                                    <a href="Dashboard.php" class="btn btn-warning btn-block">
+                                        <i class="fas fa-arrow-left"></i> Back To Dashboard
+                                    </a>
+                                </div>
+                                <div class="col-lg-6 mb-2">
+                                    <button type="button" name="Submit" class="btn btn-success btn-block">
+                                        <i class="fas fa-check"></i> Publish
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+            
+            </div> 
+        </div>
+    </section>
+    
+    
+    
+    
+    <!--- Main Area End -->
+
+
+
+
+
+    <!-- FOOTER -->
+    <div style="height: 10px; background: #27aae1;"></div>
+    <footer class="bg-dark text-white text-align">
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <p class="lead text-center">Theme By | Kurt Shiple | <span id="year"></span> &copy; ----All Rights Reserved.</p>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <div style="height: 10px; background: #27aae1;"></div>
+
+    <!-- FOOTER END -->
+
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>    
+    <script>
+        $('#year').text(new Date().getFullYear());
+    </script>
+</body>
+</html>
---
+++ includes/DB.php
@@ -0,0 +1,6 @@
+<?php
+
+$DSN='mysql:host = localhost; dbname=cms-marys-art';
+$ConnectingDB = new PDO($DSN, 'root', '');
+
+?>
