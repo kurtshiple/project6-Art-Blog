1c6c7fddbf35bd315620a3b5190c8055a2ca421a
Kurt Shiple <kurtshiple@kurts-air.attlocal.net>
2019-10-01 14:53:43 -0400

    Added functionality to the 'delete comment' button.

---
+++ deletecomments.php
@@ -0,0 +1,18 @@
+<?php require_once("includes/DB.php"); ?>
+<?php require_once("includes/Functions.php"); ?>
+<?php require_once("includes/sessions.php"); ?>
+<?php 
+if(isset($_GET["id"])){
+    $SearchQueryParameter = $_GET["id"];
+    global $ConnectingDB;
+    $sql = "DELETE FROM comments WHERE id='$SearchQueryParameter'";
+    $Execute = $ConnectingDB->query($sql);
+    if ($Execute){
+        $_SESSION["SuccessMessage"]="Comment Deleted Successfully!";
+        Redirect_to("comments.php");
+    }else{
+        $_SESSION["ErrorMessage"]="Something Went Wrong. Try Again!";
+        Redirect_to("comments.php"); 
+    }
+}
+?>
